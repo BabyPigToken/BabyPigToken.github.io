7ea3d810b1da2c43735a9965e5f85faf9bd91487
BabyPigToken <87226521+BabyPigToken@users.noreply.github.com>
2021-12-24 03:12:49 +0800

    Add files via upload

---
+++ precache-manifest.c421067d04bf1eb38957eb8937b99ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f71aac8c13586bcbf4d",
+    "url": "/static/css/main.b76ad351.chunk.css"
+  },
+  {
+    "revision": "4f71aac8c13586bcbf4d",
+    "url": "/static/js/main.a619be44.chunk.js"
+  },
+  {
+    "revision": "80d94a5509a10bbf0832",
+    "url": "/static/js/runtime~main.6f233040.js"
+  },
+  {
+    "revision": "71b672a656c53fc9c9ca",
+    "url": "/static/js/2.6491ab2f.chunk.js"
+  },
+  {
+    "revision": "46e58dcc3c92ed48c7e5",
+    "url": "/static/js/3.ff36ac53.chunk.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "2487f492afa757ec2f81e37e5c72867e",
+    "url": "/index.html"
+  }
+];
